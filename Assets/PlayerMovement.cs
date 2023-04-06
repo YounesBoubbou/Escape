55decbf9170e6d74589fcd9c56c00a2b3b206e57
YounesBoubbou <Y.Boubbou@aui.ma>
2023-04-06 16:26:02 +0000

    fixed stairs and character control mid air

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -23,6 +23,7 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetButtonDown("Jump"))
         {
             jump = true;
+
         }
         if(Input.GetButtonDown("Crouch"))
         {
